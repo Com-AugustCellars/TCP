5faa98840ebde925e4e41239fb905cb9260737eb
Jim Schaad <ietf@augustcellars.com>
2017-11-12 12:30:52 +0800

    Initial Set of files.

---
+++ TCP/TCPClientChannel.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Net;
+using System.Net.Sockets;
+using Com.AugustCellars.CoAP.Channel;
+
+namespace Com.AugustCellars.CoAP.TLS
+{
+    /// <summary>
+    /// Low level channel for DTLS when dealing only with clients.
+    /// This channel will not accept any connections from other parties.
+    /// </summary>
+    internal class TCPClientChannel : IChannel
+    {
+        public const Int32 DefaultReceivePacketSize = 4096;
+
+        private readonly System.Net.EndPoint _localEndPoint;
+        private Int32 _receiveBufferSize = DefaultReceivePacketSize;
+        private Int32 _sendBufferSize;
+        private Int32 _receivePacketSize;
+        private readonly int _port;
+
+        /// <summary>
+        /// Create a client only channel and use a randomly assigned port on
+        /// the client UDP port.
+        /// </summary>
+        public TCPClientChannel() : this(0)
+        {
+        }
+
+        /// <summary>
+        /// Create a client only channel and use a given point
+        /// </summary>
+        /// <param name="port">client side UDP port</param>
+        public TCPClientChannel(Int32 port)
+        {
+            _port = port;
+        }
+
+        /// <summary>
+        /// Create a client only channel and use a given endpoint
+        /// </summary>
+        /// <param name="ep">client side endpoint</param>
+        public TCPClientChannel(System.Net.EndPoint ep)
+        {
+            _localEndPoint = ep;
+        }
+
+        /// <inheritdoc/>
+        public event EventHandler<DataReceivedEventArgs> DataReceived;
+
+        /// <inheritdoc/>
+        public System.Net.EndPoint LocalEndPoint {
+            get => (_localEndPoint == null) ? new IPEndPoint(IPAddress.IPv6Any, _port) : null;  // M00BUG
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="Socket.ReceiveBufferSize"/>.
+        /// </summary>
+        public Int32 ReceiveBufferSize {
+            get => _receiveBufferSize;
+            set => _receiveBufferSize = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="Socket.SendBufferSize"/>.
+        /// </summary>
+        public Int32 SendBufferSize {
+            get => _sendBufferSize;
+            set => _sendBufferSize = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the size of buffer for receiving packet.
+        /// The default value is <see cref="DefaultReceivePacketSize"/>.
+        /// </summary>
+        public Int32 ReceivePacketSize {
+            get => _receivePacketSize;
+            set => _receivePacketSize = value;
+        }
+
+        private Int32 _running;
+
+
+        /// <summary>
+        /// Tell the channel to set itself up and start processing data
+        /// </summary>
+        public void Start()
+        {
+            if (System.Threading.Interlocked.CompareExchange(ref _running, 1, 0) > 0) {
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Tell the channel to stop processing data cand clean itself up.
+        /// </summary>
+        public void Stop()
+        {
+            if (System.Threading.Interlocked.Exchange(ref _running, 0) == 0)
+                return;
+
+            lock (_sessionList) {
+                foreach (TcpSession session in _sessionList) {
+                    session.Stop();
+                }
+                _sessionList.Clear();
+            }
+        }
+
+        /// <summary>
+        /// We don't do anything for this right now because we don't have sessions.
+        /// </summary>
+        /// <param name="session"></param>
+        public void Abort(ISession session)
+        {
+            return;
+        }
+
+        /// <summary>
+        /// We don't do anything for this right now because we don't have sessions.
+        /// </summary>
+        /// <param name="session"></param>
+        public void Release(ISession session)
+        {
+            return;
+        }
+
+        /// <summary>
+        /// Tell the channel to release all of it's resources
+        /// </summary>
+        public void Dispose()
+        {
+            Stop();
+        }
+
+        public void Send(byte[] data, ISession session, System.Net.EndPoint ep)
+        {
+            try {
+                //  We currently only support IP addresses with this channel.
+                //  This is a restriction is enforce from BouncyCastle where
+                //  that is the only end point that can be used.
+
+                IPEndPoint ipEndPoint = (IPEndPoint)ep;
+                TcpSession tcpSession = (TcpSession)session;
+
+                if (session == null) {
+                    tcpSession = FindSession(ipEndPoint);
+                    if (session == null) {
+
+                        //  Create a new session to send with if we don't already have one
+
+                        tcpSession = new TcpSession(ipEndPoint, null /*DataReceived*/);
+                        AddSession(tcpSession);
+
+                        tcpSession.Connect();
+
+                        tcpSession.DataReceived += DataReceived;
+                    }
+                }
+
+                //  Queue the data onto the session.
+
+                tcpSession.Queue.Enqueue(new QueueItem(null, data));
+                tcpSession.WriteData();
+            }
+            catch (Exception e) {
+#if DEBUG
+                Console.WriteLine("Error in TCPClientChannel Sending - " + e.ToString());
+#endif
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Send data through the DTLS channel to other side
+        /// </summary>
+        /// <param name="data">Data to be sent</param>
+        /// <param name="ep">Where to send it</param>
+        public void Send(byte[] data, System.Net.EndPoint ep)
+        {
+            try {
+                //  We currently only support IP addresses with this channel.
+                //  This is a restriction is enforce from BouncyCastle where
+                //  that is the only end point that can be used.
+
+                IPEndPoint ipEndPoint = (IPEndPoint)ep;
+
+                TcpSession session = FindSession(ipEndPoint);
+                if (session == null) {
+
+                    //  Create a new session to send with if we don't already have one
+
+                    session = new TcpSession(ipEndPoint,null /* DataReceived*/ );
+                    AddSession(session);
+
+                    session.Connect();
+                }
+
+                //  Queue the data onto the session.
+
+                session.Queue.Enqueue(new QueueItem(null, data));
+                session.WriteData();
+            }
+            catch (Exception e) {
+#if DEBUG
+                Console.WriteLine("Error in DTLSClientChannel Sending - " + e.ToString());
+#endif
+                throw;
+            }
+        }
+
+        private void ReceiveData(Object sender, DataReceivedEventArgs e)
+        {
+            lock (_sessionList) {
+                foreach (TcpSession session in _sessionList) {
+                    if (e.EndPoint.Equals(session.EndPoint)) {
+                        // session.ReceiveData(sender, e);
+
+                        return;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Keep track of all of the sessions that have been setup on this channel.
+        /// </summary>
+        private readonly List<TcpSession> _sessionList = new List<TcpSession>();
+
+        private void AddSession(TcpSession session)
+        {
+            lock (_sessionList) {
+                _sessionList.Add(session);
+            }
+        }
+
+        private TcpSession FindSession(IPEndPoint ipEndPoint)
+        {
+            lock (_sessionList) {
+
+                foreach (TcpSession session in _sessionList) {
+                    if (session.EndPoint.Equals(ipEndPoint))
+                        return session;
+                }
+            }
+            return null;
+        }
+
+
+        public ISession GetSession(System.Net.EndPoint ep)
+        {
+            IPEndPoint ipEndPoint = (IPEndPoint)ep;
+            return (ISession) FindSession(ipEndPoint);
+        }
+    }
+}
---
+++ TCP/TCPSession.cs
@@ -0,0 +1,207 @@
+﻿using System;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Concurrent;
+using System.Threading;
+using Com.AugustCellars.CoAP.Channel;
+using Com.AugustCellars.CoAP;
+
+namespace Com.AugustCellars.CoAP.TLS
+{
+    public class TcpSession : ISession
+    {
+        private TcpClient _client;
+        private readonly IPEndPoint _ipEndPoint;
+        private QueueItem _toSend;
+        private NetworkStream _stm;
+
+        private readonly ConcurrentQueue<QueueItem> _queue = new ConcurrentQueue<QueueItem>();
+
+        public TcpSession(IPEndPoint ipEndPoint, QueueItem toSend)
+        {
+            _ipEndPoint = ipEndPoint;
+            _toSend = toSend;
+        }
+
+        public TcpSession(TcpClient client)
+        {
+            _client = client;
+            _ipEndPoint = (IPEndPoint) client.Client.RemoteEndPoint;
+        }
+
+        public ConcurrentQueue<QueueItem> Queue { get { return _queue; } }
+
+        public NetworkStream Stream
+        {
+            get
+            {
+                if (_stm == null) _stm = _client.GetStream();
+                return _stm;
+            }
+        }
+
+        public IPEndPoint EndPoint {  get { return _ipEndPoint; } }
+
+        public bool IsReliable => true;
+
+        /// <summary>
+        /// True means that it is supported, False means that it may be supported.
+        /// </summary>
+        public bool BlockTransfer { get; set; } = true;
+
+        /// <summary>
+        /// Max message size 
+        /// </summary>
+        public int MaxSendSize { get; set; } = 1152;
+
+        public void Connect()
+        {
+            _client = new TcpClient(_ipEndPoint.AddressFamily);
+
+            _client.Connect(_ipEndPoint);
+
+            _stm = _client.GetStream();
+
+            //  Send over the capability block
+
+            SignalMessage signal = new SignalMessage(SignalCode.CSM);
+            Option op;
+            if (BlockTransfer) {
+                op = Option.Create(OptionType.Signal_BlockTransfer);
+                signal.AddOption(op);
+            }
+            
+            op = Option.Create(OptionType.Signal_MaxMessageSize);
+            op.IntValue = 1152; // 2048;
+            signal.AddOption(op);
+
+            byte[] data;
+            TLSMessageEncoder enc = new TLSMessageEncoder();
+
+            data = enc.Encode(signal); 
+
+            _stm.Write(data, 0, data.Length);
+            _stm.Flush();
+
+            //  
+
+            if (_toSend != null) {
+                _stm.Write(_toSend.Data, 0, _toSend.Length);
+                _stm.Flush();
+                _toSend = null;
+            }
+
+            new Thread(() => StreamListener()).Start();
+        }
+
+        public void Stop()
+        {
+        }
+
+        public void Release()
+        {
+        }
+
+        public void Abort()
+        {
+            return;
+        }
+
+        private Int32 _writing;
+        private readonly Object _writeLock = new Object();
+
+        public event EventHandler<SessionEventArgs> SessionEvent;
+
+        public void WriteData()
+        {
+            if (_queue.Count == 0) return;
+            lock (_writeLock) {
+                if (_writing > 0) return;
+                _writing = 1;
+            }
+
+            while (Queue.Count > 0) {
+                QueueItem q;
+                if (!_queue.TryDequeue(out q)) break;
+
+                _stm.Write(q.Data, 0, q.Data.Length);
+            }
+
+            lock (_writeLock) {
+                _writing = 0;
+                if (_queue.Count > 0) WriteData();
+            }
+        }
+
+        private void StreamListener()
+        {
+            try {
+
+                NetworkStream stream = _client.GetStream();
+
+                byte[] bytes = new byte[2048+1024];
+                int offset = 0;
+                int messageSize;
+
+                while (true) {
+                    int i = stream.Read(bytes, offset, bytes.Length - offset);
+                    i += offset;
+
+                    while (i > 0) {
+                        //  Do I have a full record?
+
+                        int dataSize = (bytes[0] >> 4) & 0xf;
+                        switch (dataSize) {
+                            case 13:
+                                messageSize = bytes[1] + 13 + 3;
+                                break;
+
+                            case 14:
+                                messageSize = (bytes[1] * 256 + bytes[2]) + 269 + 4;
+                                break;
+
+                            case 15:
+                                messageSize = ((bytes[1] * 256 + bytes[2]) * 256 + bytes[3]) * 256 + bytes[4] + 65805 + 6;
+                                break;
+
+                            default:
+                                messageSize = dataSize + 2;
+                                break;
+                        }
+                        messageSize += (bytes[0] & 0xf); // Add token buffer
+
+                        if (i >= messageSize) {
+                            byte[] message = new byte[messageSize];
+                            Array.Copy(bytes, message, messageSize);
+                            Array.Copy(bytes, messageSize, bytes, 0, i - messageSize);
+                            offset = i - messageSize;
+                            i -= messageSize;
+
+                            FireDataReceived(message, EndPoint, this);
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                }
+
+            }
+            catch (Exception e) {
+                Console.WriteLine("StreamListener --> " + e.ToString());
+            }
+        }
+
+        /// <inheritdoc/>
+        public event EventHandler<DataReceivedEventArgs> DataReceived;
+
+        private void FireDataReceived(Byte[] data, System.Net.EndPoint ep, TcpSession tcpSession)
+        {
+            EventHandler<DataReceivedEventArgs> h = DataReceived;
+            if (h != null) {
+                h(this, new DataReceivedEventArgs(data, ep, tcpSession));
+            }
+        }
+
+    }
+}
